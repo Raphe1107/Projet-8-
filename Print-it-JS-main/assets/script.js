98b7bab9270f16becc4219f3a4048ec43655dd2d
Raphe1107 <rnuguet@gmail.com>
2023-06-05 16:47:56 +0200

    update

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -18,22 +18,74 @@ const slides = [
 ]
 
 
+// On récupècure nos éléments dans le HTML
 const flecheGauche = document.querySelector (".arrow_left");
 const flecheDroite = document.querySelector (".arrow_right");
-const Point = document.querySelector (".dot");
-const PointSelec = document.querySelector (".dot_selected");
+const tagLine = document.querySelector ("#banner p");
+const Image = document.querySelector (".banner-img");
+const dots = document.querySelector (".dot");
+
+document.querySelector(".dots").innerHTML = 
+'<div class="dot"></div>'
++'<div class="dot"></div>'
++'<div class="dot"></div>'
++'<div class="dot"></div>';
+
+// On défini l'index de la diapositive actuelle
+let currentSlide = 0;
+
+// Fonction qui permet d'afficher la diapositive actuelle 
+function showSlide() {
+
+// On récupère l'objet de la diapositive correspondante à l'index actuel
+const currentSlideObj = slides[currentSlide];
 
 
+// On met à jour l'image de la diapositive
+Image.src = `./assets/images/slideshow/${currentSlideObj.image}`;
 
+// On met à jour la tagLine de l'image 
+tagLine.innerHTML = currentSlideObj.tagLine;
+
+};
+
+// On ajoute un évenement sur la flèche de gauche 
 flecheGauche.addEventListener ("click" , ()=> {
-banner.style.background =  "green";
-console.log(flecheGauche);
-})
+	// On décrémente l'index de la diapositive actuelle 
+	currentSlide--;
 
+	// On vérifie avec la condition if si l'index est inférieur à 0 et donc revenir à la dernière diapositive
+	if (currentSlide <0) {
+		currentSlide = slides.length - 1;
+	}
 
+	// On affiche la diapostive mise à jour
+	showSlide();
+	
+});
 
+// On ajoute un évenement sur la flèche de droite
 flecheDroite.addEventListener ("click" , ()=> {
-banner.style.background = "red";
-console.log(flecheDroite);
-})
+	// On incrémente l'index de la diapositive actuelle 
+	currentSlide ++;
+
+	// On vérifie si l'index dépasse la dernière diapositive
+	if (currentSlide >= slides.length) {
+		currentSlide = 0; // On réinitialise l'index à 0 
+	}
+
+	// On affiche la diapostive mise à jour
+	showSlide();
+});
+
+showSlide();
+
+
+
+
+
+
+
+
+
 
